3e03384f78581bb558dd0725ede0fb02dc0c6551
Aiman Daniel <aiman2301@gmail.com>
2026-07-06 00:42:49 +0800

    lambda-lab: default region

--- lambda-lab/variables.tf
+++ lambda-lab/variables.tf
@@ -1,5 +1,5 @@
 variable "aws_region" {
   type        = string
-  default     = "ap-southeast-5"
+  default     = "ap-southeast-1"
   description = "AWS region. Must support Lambda Function URLs."
 }
